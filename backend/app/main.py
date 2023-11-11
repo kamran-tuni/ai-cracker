3ab0665a7aacfd32b64c7b6fd9aef8946cf131e0
kamran-tuni <119616926+kamran-tuni@users.noreply.github.com>
2023-11-11 16:39:22 +0200

    Add data point listing api (#2)

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,6 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="ChatOTK Documentation", version="0.0.1")
 app.include_router(api_router, prefix="/api/v1")
+app.include_router(api_router, prefix="/api/v1")
 
 origins = [
     "http://localhost:8000",
--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,2 +1,3 @@
 from .base import Base
 from .app_config import AppConfig
+from .data_point import DataPoint
