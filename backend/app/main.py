fb9b9f3c1640d66e78593057cac949928da12f39
Muhammad Kamran <muhammmad.kamran@tuni.fi>
2023-11-11 22:33:09 +0200

    Fix cors issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,6 +10,8 @@ app.include_router(api_router, prefix="/api/v1")
 
 origins = [
     "http://localhost:8000",
+    "http://localhost:4200",
+    "http://5.22.219.30:4200/"
 ]
 
 app.add_middleware(
