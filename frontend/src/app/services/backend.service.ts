eced796eca85d50b8c9013ee46958f337bac5b9a
Eetu Hyyryläinen <eetu.hyyrylainen@siilim221004.local>
2023-11-11 23:12:52 +0200

    Fix integration

--- frontend/src/app/services/backend.service.ts
+++ frontend/src/app/services/backend.service.ts
@@ -9,15 +9,21 @@ export type Conversation = {
 }
 
 export type News = {
+  article_id?: number
   source: string
-  headline: string
+  heading: string
   summary: string
+  relevance_score: number
+  sentiment_score: number
 }
 
 export type Configuration = {
   news_sources: string[]
-  keywords: string[],
+  keywords: string[]
   industries: string[]
+  ai_model_endpoint?: string
+  ai_model_version?: string
+  ai_model_api_key?: string
 }
 
 @Injectable({
@@ -75,14 +81,12 @@ export class BackendService {
   private setNews(): Promise<any> {
     const url = 'http://5.22.219.30:8000/api/v1/data-points/';
     return firstValueFrom(this.http.get(url)).then((resp: any) => {
-      try {
-        this._news.next(resp.result);
-      } finally {
-        this.setMockNews();
-      }
+      const news = resp.result;
+      this._news.next(news);
       return Promise.resolve();
     },
     () => {
+      console.log('errorissa');
       this.setMockNews();
     })
   }
@@ -101,6 +105,10 @@ export class BackendService {
 
   setConfiguration(configuration: Configuration): Promise<any> {
     this.startChat();
+    // Backend needs these... from an old design
+    configuration.ai_model_endpoint = '';
+    configuration.ai_model_version = '';
+    configuration.ai_model_api_key = '';
     const url = 'http://5.22.219.30:8000/api/v1/config/';
     return firstValueFrom(this.http.post(url, configuration)).finally(() => {
       return this.setNews();
@@ -133,28 +141,38 @@ export class BackendService {
         this._news.next([
           {
             source: 'News Metal',
-            headline: "Despite recent recovery of digital electronic market, LCO makers hold gloomy market outlook for December",
-            summary: "The share of foreign-language speakers of the population of Helsinki stood at 13.5 per cent at the beginning of this year, with Russian, Estonian, Somali and English speakers being the largest groups foreign-language speakers."
+            heading: "Despite recent recovery of digital electronic market, LCO makers hold gloomy market outlook for December",
+            summary: "The share of foreign-language speakers of the population of Helsinki stood at 13.5 per cent at the beginning of this year, with Russian, Estonian, Somali and English speakers being the largest groups foreign-language speakers.",
+            relevance_score: 10,
+            sentiment_score: -0.5
           },
           {
             source: 'Helsingin Sanomat',
-            headline: "Foreign-language speakers to account for almost 25% of Helsinki's population",
-            summary: "Arabic, in particular, is projected to become a greater feature of the urban landscape. Helsinki will according to the forecast be home to as many as 32,000 people who speak Middle Eastern or Northern African languages – such as Arabic or Kurdish – as their mother tongue by 2030, representing a three-fold increase from the current situation."
+            heading: "Foreign-language speakers to account for almost 25% of Helsinki's population",
+            summary: "Arabic, in particular, is projected to become a greater feature of the urban landscape. Helsinki will according to the forecast be home to as many as 32,000 people who speak Middle Eastern or Northern African languages – such as Arabic or Kurdish – as their mother tongue by 2030, representing a three-fold increase from the current situation.",
+            relevance_score: 10,
+            sentiment_score: -0.5
           },
           {
             source: 'News Metal',
-            headline: "Nickel sulphate prices plunge due to a simultaneous weakening of fundamentals and cost-side supports",
-            summary: ""
+            heading: "Nickel sulphate prices plunge due to a simultaneous weakening of fundamentals and cost-side supports",
+            summary: "",
+            relevance_score: 10,
+            sentiment_score: -0.5
           },
           {
             source: 'Routers',
-            headline: "Silicon metal prices continue to fall, but may stabilise cost support",
-            summary: ""
+            heading: "Silicon metal prices continue to fall, but may stabilise cost support",
+            summary: "",
+            relevance_score: 10,
+            sentiment_score: -0.5
           },
           {
             source: 'News Metal',
-            headline: "Rather Than Battling Against Layers Of Earth Thousands Of Meters Deep, Delving Deeper Into The Improvement Of Mining Techniques May Be One Of The Best Paths Towards Maximizing The Value Of Domestic Mining Resources In The Future",
-            summary: ""
+            heading: "Rather Than Battling Against Layers Of Earth Thousands Of Meters Deep, Delving Deeper Into The Improvement Of Mining Techniques May Be One Of The Best Paths Towards Maximizing The Value Of Domestic Mining Resources In The Future",
+            summary: "",
+            relevance_score: 10,
+            sentiment_score: -0.5
           }
         ]);
         resolve();
